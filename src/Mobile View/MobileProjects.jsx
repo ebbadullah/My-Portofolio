d9aae8128840114b8e17475e3975e661e8316cac
Ebad Baloch <ebadbaloch345@gmail.com>
2025-09-01 11:27:10 +0500

    fixed  import  issue

--- src/Mobile View/MobileProjects.jsx
+++ src/Mobile View/MobileProjects.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { ExternalLinkIcon, GithubIcon } from "../Components/Icons"
-import { Data } from "../../assets/Data/Data"
+import { Data } from "../assets/Data/Data"
 
 export default function MobileProjects() {
     const [projects] = useState([
